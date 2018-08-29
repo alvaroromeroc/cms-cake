5a3b5e8e2a63a7dca5d1efccda86c5c4a91e83f7
AlvaroRomeroUsach <alvaro.romero@usach.cl>
2018-08-29 11:54:27 -0300

    Primer commit - Basic CRUD para artículos

---
+++ src/Model/Table/ArticlesTable.php
@@ -0,0 +1,36 @@
+<?php
+// src/Model/Table/ArticlesTable.php
+namespace App\Model\Table;
+
+use Cake\ORM\Table;
+use Cake\Utility\Text; // para pasar el titulo a slug
+use Cake\Validation\Validator; // Para el metodo de validación 
+
+class ArticlesTable extends Table
+{
+    public function initialize(array $config)
+    {
+        $this->addBehavior('Timestamp');
+    }
+
+    public function beforeSave($event, $entity, $options)
+	{
+	    if ($entity->isNew() && !$entity->slug) {
+	        $sluggedTitle = Text::slug($entity->title);
+	        // trim slug to maximum length defined in schema
+	        $entity->slug = substr($sluggedTitle, 0, 191);
+	    }
+	}
+
+	public function validationDefault(Validator $validator)
+	{
+	    $validator
+	        ->notEmpty('title')
+	        ->minLength('title', 10)
+	        ->maxLength('title', 255)
+	        ->notEmpty('body')
+	        ->minLength('body', 10);
+
+	    return $validator;
+	}
+}
